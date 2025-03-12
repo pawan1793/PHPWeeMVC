923a5e744886b360ac7b2ea6881bf917a83058d3
Pawan More <pawanmore38@gmail.com>
2025-03-12 11:45:19 +0530

    env updated

--- config/database.php
+++ config/database.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'database' => 'robo',
-    'username' => 'root',
-    'password' => 'root',
+    'driver'    => env('DB_CONNECTION', 'mysql'),
+    'host'      => env('DB_HOST', '127.0.0.1'),
+    'port'      => env('DB_PORT', '3306'),
+    'database'  => env('DB_DATABASE', 'my_database'),
+    'username'  => env('DB_USERNAME', 'root'),
+    'password'  => env('DB_PASSWORD', ''),
+    'charset'   => env('DB_CHARSET', 'utf8mb4'),
+    'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
 ];
